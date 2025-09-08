33d3dc0088c61d103ca930b89acce33b89ebdc66
eduantunex <antunesedu.digital@gmail.com>
2025-09-08 14:16:21 -0300

    funcao regprod criada, pagina inicial criada

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from colorama import Fore, Back, Style, init
 import sqlite3 as sq
 import os
 import platform
+import time
 
 clear = ""
 sistema = platform.system()
@@ -31,4 +32,54 @@ cursor.execute("CREATE TABLE IF NOT EXISTS vendas (valor REAL, nome TEXT, quanti
 def registrar_prod():
     os.system(clear)
     titulo()
-    print(Fore.BLUE)
+    print(Fore.BLUE + "Digite o nome do produto:")
+    nome_produto = input(">>> ")
+    def try_valorproduto():
+        os.system(clear)
+        titulo()
+        print(Fore.BLUE + "Digite o valor do produto:")
+        try:
+            valor_produto = float(input(">>> "))
+        except:
+            print(Fore.YELLOW + "Você deve digitar um NUMERO")
+            time.sleep(1)
+            try_valorproduto()
+        return valor_produto
+    valor_produto = try_valorproduto()
+    def try_estoqueproduto():
+        os.system(clear)
+        titulo()
+        print(Fore.BLUE + "Digite a quantidade em estoque:")
+        try:
+            estoque_produto = float(input(">>>"))
+            return estoque_produto
+        except:
+            print(Fore.YELLOW + "Você deve digitar um NUMERO")
+            time.sleep(1)
+            try_estoqueproduto()
+    estoque_produto = try_estoqueproduto()
+
+    cursor.execute("INSERT INTO produtos (valor, nome, quantidade) VALUES (?,?,?)", (valor_produto, nome_produto, estoque_produto))
+    conn.commit()
+
+
+while True:
+    os.system(clear)
+    titulo()
+    print(Fore.BLUE + """Selecione uma opção:
+          (1) Registrar uma Venda
+          (2) Ver Produtos
+          (3) Ver Clientes""")
+    prompt = input(">>> ")
+    if prompt == "1":
+        print(Fore.YELLOW + "Sem sucesso...")
+        time.sleep(1)
+    elif prompt == "2":
+        print(Fore.YELLOW + "Sem sucesso...")
+        time.sleep(1)
+    elif prompt == "3":
+        print(Fore.YELLOW + "Sem sucesso...")
+        time.sleep(1)
+    else:
+        print(Fore.YELLOW + "Escreva uma opção correta...")
+        time.sleep(1)
